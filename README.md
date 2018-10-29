3e4f2c0877fbea6d7430de1d53fcd8151a26b978
LikesunnyLikerain <812492664@qq.com>
2018-10-29 23:32:30 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # test
-编辑器：HBuilder/sublime/微信web开发者工具
-希望这次比赛能得到锻炼
---
+++ test.js
@@ -0,0 +1,8 @@
+/*
+* @Author: anchen
+* @Date:   2018-10-29 21:21:47
+* @Last Modified by:   anchen
+* @Last Modified time: 2018-10-29 21:50:51
+*/
+
+'use strict';
